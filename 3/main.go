4d880556a2bef04675f3a974eef3eded4204ef9c
Mitch <mitch.hines@cyndicatelabs.co.uk>
2024-12-06 17:58:11 +0000

    3 part b

--- 3/main.go
+++ 3/main.go
@@ -6,16 +6,10 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func main() {
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("[+] Data size: %d\n", len(data))
-
+func partA(data []byte) {
 	totalSum := 0
 	totalMatches := 0
 
@@ -29,4 +23,59 @@ func main() {
 
 	fmt.Printf("[+] Total matches: %d\n", totalMatches)
 	fmt.Printf("[+] Total sum: %d\n", totalSum)
+}
+
+func partB(data []byte) {
+
+	totalSum := 0
+
+	r, _ := regexp.Compile(`mul\((\d+),(\d+)\)`)
+	rDont, _ := regexp.Compile(`(don\'t\(\)){1}`)
+
+	data_str := string(data)
+	blocks := strings.Split(data_str, "do()")
+
+	for _, block := range blocks {
+
+		fmt.Printf("\n\033[1;32m[+]\033[0m Block: %s\n", block)
+
+		muls := r.FindAllStringIndex(block, -1)
+		donts := rDont.FindAllStringIndex(block, -1)
+
+		for _, mul := range muls {
+			if len(donts) > 0 {
+				if mul[1] <= donts[0][0] {
+					mulStr := []byte(block[mul[0]:mul[1]])
+					for _, match := range r.FindAllSubmatch(mulStr, -1) {
+						num1, _ := strconv.Atoi(string(match[1]))
+						num2, _ := strconv.Atoi(string(match[2]))
+						totalSum += num1 * num2
+						fmt.Printf("\033[1;33m[+]\033[0m mul(%d, %d) = %d\n", num1, num2, num1*num2)
+					}
+				}
+			} else {
+				mulStr := []byte(block[mul[0]:mul[1]])
+				for _, match := range r.FindAllSubmatch(mulStr, -1) {
+					num1, _ := strconv.Atoi(string(match[1]))
+					num2, _ := strconv.Atoi(string(match[2]))
+					totalSum += num1 * num2
+					fmt.Printf("\033[1;33m[+]\033[0m mul(%d, %d) = %d\n", num1, num2, num1*num2)
+				}
+			}
+		}
+	}
+	fmt.Printf("[+] Total sum: %d\n", totalSum)
+}
+
+func main() {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("[+] Data size: %d\n", len(data))
+
+	partA(data)
+	partB(data)
+
 }
